2b4bc15b25a33d0eba75840843039376ac8ef72a
github2020rihani <86105283+github2020rihani@users.noreply.github.com>
2021-10-12 10:51:04 +0100

    fix bug 1210

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -12,7 +12,7 @@ class DashboardController extends AbstractController
 
 
     /**
-     * @Route("/super_admin", name="dashboard_super_admin")
+     * @Route("/super_admin/", name="dashboard_super_admin")
      */
     public function dashboard()
     {
@@ -20,7 +20,7 @@ class DashboardController extends AbstractController
         return $this->render('dashboard/dashboard_super_admin.html.twig');
     }
     /**
-     * @Route("/admin", name="dashboard_admin")
+     * @Route("/admin/", name="dashboard_admin")
      */
     public function dashboardAdmin()
     {
@@ -28,7 +28,7 @@ class DashboardController extends AbstractController
         return $this->render('dashboard/dashboard_admin.html.twig');
     }
     /**
-     * @Route("/responsable", name="dashboard_responsable")
+     * @Route("/responsable/", name="dashboard_responsable")
      */
     public function dashboardResponsable()
     {
@@ -37,7 +37,7 @@ class DashboardController extends AbstractController
     }
 
     /**
-     * @Route("/gerant", name="dashboard_gerant")
+     * @Route("/gerant/", name="dashboard_gerant")
      */
     public function dashboardGerant()
     {
